c6ce7d114f142127bb0efc14c115e0404a9b2199
sowasred <muldurozan@gmail.com>
2020-06-22 17:19:26 -0400

    resume updated

--- js/app.js
+++ js/app.js
@@ -3,12 +3,16 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const resume = document.querySelector(".resume");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
     navbar.classList.add("navbar-fixed");
+    resume.classList.remove("resume");
   } else {
     navbar.classList.remove("navbar-fixed");
+    resume.classList.add("resume");
   }
 });
 // show sidebar
